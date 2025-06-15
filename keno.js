443f40fb50aa1cf61152a568a1e76f63891debd2
int0214 <jaeyuljung0@gmail.com>
2025-06-15 12:24:56 +0900

    Update keno.js

--- keno.js
+++ keno.js
@@ -1,102 +1,117 @@
-function roll(){
-  let a = "<font color='#ffffff'>"
-  let games = Number(document.getElementById("games").value)
-  let wild = Number(document.getElementById("wild").value)
-  let level = Number(document.getElementById("level").value)
-  let diceSides = Number(document.getElementById("sides").value)
+function roll() {
+  let a = "<font color='#ffffff'>";
+  let games = Number(document.getElementById("games").value);
+  let wild = Number(document.getElementById("wild").value);
+  let level = Number(document.getElementById("level").value);
+  let diceSides = Number(document.getElementById("sides").value);
+  let playerChoice = Number(document.getElementById("playerChoice").value);
   
-  let payoutPerHit = wild * level
-  let playerDice = level+4
-  let dealerDice = level+14
-  let totalPayout = 0
-  let results = ""
+  let payoutPerHit = wild * level;
+  let playerDice = level + 4;
+  let dealerDice = level + 14;
+  let totalPayout = 0;
+  let results = "";
   let gamesPlayed = 0;
-  for (let game = 1; game <= games; game++){
-    let payout = -(playerDice * level)
-    let hits = 0
-    let playerArray = generatePArray(playerDice, diceSides)
-    let dealerArray = generateDArray(dealerDice, diceSides)
-    playerArray = playerArray.sort((a,b)=>b-a)
-    if (document.getElementById("rmult").checked){
-      let playerhits = []
-      for (let i of dealerArray[0].concat(dealerArray[1])){
-        if (playerArray.includes(i)){
-        if (!playerhits.includes(i)){
-          playerhits.push(i)
-          payout += payoutPerHit
-        } else {
-          payout += payoutPerHit*3/2
-        }
+
+  for (let game = 1; game <= games; game++) {
+    let payout = -(playerDice * level);
+    let hits = 0;
+    let playerArray = generatePArray(playerDice, diceSides, playerChoice);
+    let dealerArray = generateDArray(dealerDice, diceSides);
+    playerArray = playerArray.sort((a, b) => b - a);
+
+    if (document.getElementById("rmult").checked) {
+      let playerhits = [];
+      for (let i of dealerArray[0].concat(dealerArray[1])) {
+        if (playerArray.includes(i)) {
+          if (!playerhits.includes(i)) {
+            playerhits.push(i);
+            payout += payoutPerHit;
+          } else {
+            payout += payoutPerHit * 3 / 2;
+          }
         }
       }
-    }else {
-    for (let i in playerArray) {
-      if (dealerArray[0].includes(playerArray[i]) || dealerArray[1].includes(playerArray[i])){
-        payout += payoutPerHit
-        if (i > 0){
-        if (document.getElementById("fmult").checked && playerArray[i] == playerArray[i-1])
-          payout += payoutPerHit/2
+    } else {
+      for (let i in playerArray) {
+        if (dealerArray[0].includes(playerArray[i]) || dealerArray[1].includes(playerArray[i])) {
+          payout += payoutPerHit;
+          if (i > 0) {
+            if (document.getElementById("fmult").checked && playerArray[i] == playerArray[i - 1])
+              payout += payoutPerHit / 2;
+          }
         }
       }
     }
-    }
-    for (let i of dealerArray[0]){
-      if (playerArray.includes(i)){
+
+    for (let i of dealerArray[0]) {
+      if (playerArray.includes(i)) {
         hits++;
       }
     }
-    for (let i of dealerArray[1]){
-      if (playerArray.includes(i)){
+    for (let i of dealerArray[1]) {
+      if (playerArray.includes(i)) {
         hits++;
       }
     }
-    payout = Math.floor(payout)
-    totalPayout += payout
-    dealerArray[0] = dealerArray[0].sort((a,b)=>b-a)
+    payout = Math.floor(payout);
+    totalPayout += payout;
+    dealerArray[0] = dealerArray[0].sort((a, b) => b - a);
     gamesPlayed++;
     if (games <= 10000 || game == games || hits >= playerDice)
-    results += "Wild Cards: "+playerArray.join(", ")+"; Dealer Rolls: "+dealerArray[0].join(", ")+(dealerArray[1].length>0?", Dealer Rerolls: "+dealerArray[1].join(", "):"")+". Payout: "+payout+", Hits: "+hits+"<br>"
-    if (hits >= playerDice){
+      results += "Wild Cards: " + playerArray.join(", ") + "; Dealer Rolls: " + dealerArray[0].join(", ") + (dealerArray[1].length > 0 ? ", Dealer Rerolls: " + dealerArray[1].join(", ") : "") + ". Payout: " + payout + "<br>";
+    if (hits >= playerDice) {
       break;
     }
   }
-  if (games <= 10000) results = "Total Payout: "+totalPayout+"<br>"+results
-  else  results = "Total Payout: "+totalPayout+"<br>Final Game result:<br>"+results+"<br>Games Played: "+gamesPlayed
-  if (wild < level+4) {
-    results = "You do not have enough wild cards to automate keno"
+  if (games <= 10000) results = "Total Payout: " + totalPayout + "<br>" + results;
+  else results = "Total Payout: " + totalPayout + "<br>Final Game result:<br>" + results + "<br>Games Played: " + gamesPlayed;
+  if (wild < level + 4) {
+    results = "You do not have enough wild cards to automate keno";
   }
   document.getElementById("results").innerHTML = results;
 }
 
-function generatePArray(diceAmt, sides) {
-  let arr = []
-  for (let i = 0; i < diceAmt; i++){
-    let x = Math.floor(Math.random()*sides)+1
-    if (!arr.includes(x) || document.getElementById("fmult").checked){arr.push(x)}
-    else {
-      while (arr.includes(x)){
-        x = Math.floor(Math.random()*sides)+1
+function generatePArray(diceAmt, sides, playerChoice) {
+  let arr = [];
+  // Always include the player's chosen number if valid
+  if (
+    typeof playerChoice === "number" &&
+    !isNaN(playerChoice) &&
+    playerChoice > 0 &&
+    playerChoice <= sides
+  ) {
+    arr.push(playerChoice);
+  }
+  for (let i = arr.length; i < diceAmt; i++) {
+    let x = Math.floor(Math.random() * sides) + 1;
+    if (!arr.includes(x) || document.getElementById("fmult").checked) {
+      arr.push(x);
+    } else {
+      while (arr.includes(x)) {
+        x = Math.floor(Math.random() * sides) + 1;
       }
-      arr.push(x)
+      arr.push(x);
     }
   }
-  return arr
+  return arr;
 }
 
 function generateDArray(diceAmt, sides) {
-  let arr = [[],[]]
-  let rerolls = 0
-  for (let i = 0; i < diceAmt; i++){
-    let newnum = Math.floor(Math.random()*sides)+1
-    if (arr[0].includes(newnum)) rerolls++
-    arr[0].push(newnum)
+  let arr = [[], []];
+  let rerolls = 0;
+  for (let i = 0; i < diceAmt; i++) {
+    let newnum = Math.floor(Math.random() * sides) + 1;
+    if (arr[0].includes(newnum)) rerolls++;
+    arr[0].push(newnum);
   }
   while (rerolls > 0) {
-    let newnum = Math.floor(Math.random()*sides)+1
-    if (arr[0].includes(newnum) || arr[1].includes(newnum)){rerolls++}
-    arr[1].push(newnum)
-    rerolls--
-    
+    let newnum = Math.floor(Math.random() * sides) + 1;
+    if (arr[0].includes(newnum) || arr[1].includes(newnum)) {
+      rerolls++;
+    }
+    arr[1].push(newnum);
+    rerolls--;
   }
-  return arr
+  return arr;
 }
